4fbaac101904d7d5093bc20c8cf6eef0405f3bd4
Hamid <iritmanir@gmail.com>
2021-05-29 20:17:42 +0430

    Domain exception created.

---
+++ 00 Framework/Framework.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Framework.Domain.Exceptions
+{
+    [Serializable]
+    public class DomainException : Exception
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+    }
+}
