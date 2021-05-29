8f5b088e8ef92755c15c9c6cf290e39146018df1
Hamid <iritmanir@gmail.com>
2021-05-29 22:10:58 +0430

    Base url added to appsetting

--- 03 EndPoints/UrlShortener.EndPoints.API/Configuration/ServiceConfig.cs
+++ 03 EndPoints/UrlShortener.EndPoints.API/Configuration/ServiceConfig.cs
@@ -4,6 +4,7 @@
     {
         public string Id { get; set; } = "Service01";
         public int CacheDuration { get; set; }
+        public string BaseUrl { get; set; }
         public SwaggerConfig Swagger { get; set; }
     }
 }
---
+++ 03 EndPoints/UrlShortener.EndPoints.API/Extension/HttpContextExtension.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using UrlShortener.EndPoints.API.Configuration;
+
+namespace UrlShortener.EndPoints.API.Extension
+{
+    public static class HttpContextExtension
+    {
+        public static ServiceConfig ServiceContext(this HttpContext httpContext) =>
+            (ServiceConfig)httpContext.RequestServices.GetService(typeof(ServiceConfig));
+    }
+}
