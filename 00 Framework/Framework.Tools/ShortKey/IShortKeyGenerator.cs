346b7bd374670078690c70fb2f77a5b95ae453fc
Hamid <iritmanir@gmail.com>
2021-05-29 21:53:04 +0430

    Short key generater created.

---
+++ 00 Framework/Framework.Tools/ShortKey/IShortKeyGenerator.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Framework.Tools.ShortKey
+{
+    public interface IShortKeyGenerator
+    {
+        Task<string> GenerateUniqKey();
+    }
+}
---
+++ 00 Framework/Framework.Tools/ShortKey/ShortKeyGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Framework.Tools.ShortKey
+{
+    public class ShortKeyGenerator : IShortKeyGenerator
+    {
+        public async Task<string> GenerateUniqKey()
+        {
+            return "TestKey";
+        }
+    }
+}
