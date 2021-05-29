5b49d62e9f88f8dbeaefa651627da687a4d62621
Hamid <iritmanir@gmail.com>
2021-05-29 22:09:51 +0430

    Url handlers created.

---
+++ 01 Core/04 UrlShortener.ApplicationServices/UrlAgg/CreateShortUrlHandler.cs
@@ -0,0 +1,33 @@
+﻿using Framework.Tools.ShortKey;
+using System;
+using System.Threading.Tasks;
+using UrlShortener.ApplicationServices._Base;
+using UrlShortener.Contracts._Base;
+using UrlShortener.DomainModels.UrlAgg.Dtoes;
+using UrlShortener.DomainModels.UrlAgg.Entities;
+using UrlShortener.DomainModels.UrlAgg.Requests;
+using UrlShortener.DomainModels.UrlAgg.ValueObjects;
+
+namespace UrlShortener.ApplicationServices.UrlAgg
+{
+    public class CreateShortUrlHandler : RequestHandlerAsync<CreateShortUrl, ShortUrlDto>
+    {
+        private readonly IShortKeyGenerator _shortKeyGenerator;
+        public CreateShortUrlHandler(IUnitOfWork unitOfWork, IShortKeyGenerator shortKeyGenerator) : base(unitOfWork)
+        {
+            _shortKeyGenerator = shortKeyGenerator;
+        }
+
+        public override async Task<ShortUrlDto> HandleAsync(CreateShortUrl req)
+        {
+            var longUrl = new UrlLongUrl(req.LongUrl);
+            var shortUrl = await _shortKeyGenerator.GenerateUniqKey();
+            var url = new Url(Guid.NewGuid(), new UrlShortKey(shortUrl), longUrl);
+            
+            //await UnitOfWork.UrlRepository.AddAsync(url);
+            //await UnitOfWork.CommitAsync();
+            
+            return new ShortUrlDto(shortUrl);
+        }
+    }
+}
---
+++ 01 Core/04 UrlShortener.ApplicationServices/UrlAgg/GetOriginalUrlHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Framework.Domain.Exceptions;
+using System.Threading.Tasks;
+using UrlShortener.ApplicationServices._Base;
+using UrlShortener.Contracts._Base;
+using UrlShortener.DomainModels.UrlAgg.Dtoes;
+using UrlShortener.DomainModels.UrlAgg.Entities;
+using UrlShortener.DomainModels.UrlAgg.Requests;
+using UrlShortener.DomainModels.UrlAgg.ValueObjects;
+
+namespace UrlShortener.ApplicationServices.UrlAgg
+{
+    public class GetOriginalUrlHandler : RequestHandlerAsync<GetOriginalUrl, OriginalUrlDto>
+    {
+        public GetOriginalUrlHandler(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public override async Task<OriginalUrlDto> HandleAsync(GetOriginalUrl req)
+        {
+            var shortKey = new UrlShortKey(req.ShortKey);
+
+            //todo => update unitOfWork
+            //var url = await UnitOfWork.UrlRepository.GetAsync(shortKey.Value);
+            var url = new Url(Guid.NewGuid(), shortKey, new UrlLongUrl("http://google.com"));
+            if (url is null)
+                throw new DomainException("Url not found.");
+
+            return new OriginalUrlDto(url.LongUrl.Value);
+        }
+    }
+}
