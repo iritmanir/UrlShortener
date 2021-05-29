00c913c4cd9097d84caf48acd417a781fd1944cb
Hamid <iritmanir@gmail.com>
2021-05-29 22:07:46 +0430

    GetOriginalUrl request added.

---
+++ 01 Core/01 UrlShortener.DomainModels/UrlAgg/Requests/GetOriginalUrl.cs
@@ -0,0 +1,14 @@
+﻿using Framework.Domain.Requests;
+
+namespace UrlShortener.DomainModels.UrlAgg.Requests
+{
+    public class GetOriginalUrl : IRequest
+    {
+        public string ShortKey { get; set; }
+
+        public GetOriginalUrl(string shortKey)
+        {
+            ShortKey = shortKey;
+        }
+    }
+}
